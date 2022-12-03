5ca6bfc61e83f0409419966e43d54b4b6aa72e42
leeharyung <lhr4884@naver.com>
2022-12-04 04:29:45 +0900

    style: setting theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,63 @@
+export const colors = {
+  yellow: '#FED049',
+  darkGreen: '#3D5656',
+  green: '#68B984',
+  mint: '#CFFDE1',
+  lightGrey: '#A1A1A1',
+  grey: '#D9D9D9',
+};
+
+export const fonts = {
+  h1: `
+    font-size: 2.4rem;
+    font-weight: bold;
+  `,
+  h2: `
+    font-size: 2rem;
+    font-weight: bold;
+  `,
+  h3: `
+    font-size: 1.8rem;
+    font-weight: bold;
+  `,
+  h4: `
+    font-size: 1.6rem;
+    font-weight: 500;
+  `,
+  h5: `
+    font-size: 1.4rem;
+    font-weight: bold;
+  `,
+  h6: `
+    font-size: 1.4rem;
+    font-weight: 500;
+  `,
+  subtitle1: `
+    font-size: 2.4rem;
+    font-weight: 500;
+    line-height: 144%;
+  `,
+  body: `
+    font-size: 1.4rem;
+    font-weight: 500;
+    line-height: 170%;
+  `,
+  caption1: `
+    font-size: 1.2rem;
+    font-weight: 500;
+  `,
+  caption2: `
+    font-size: 1.2rem;
+    font-weight: bold;
+  `,
+  btn1: `
+    font-size: 1.6rem;
+    font-weight: bold;
+    line-height: 1.8rem;
+  `,
+  btn2: `
+    font-size: 1.4rem;
+    font-weight: 500;
+    line-height: 1.6rem;
+  `,
+};
