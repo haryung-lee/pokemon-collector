86397bd26e3c272ca7518555311810a6d440cd8f
leeharyung <lhr4884@naver.com>
2022-12-05 02:32:59 +0900

    refactor: separate skeleton component

--- src/components/Main/PokemonCard.tsx
+++ src/components/Main/PokemonCard.tsx
@@ -2,70 +2,7 @@ import styled from 'styled-components';
 import { usePokemon } from 'hooks/usePokemon';
 import { colors, fonts } from 'styles/theme';
 import { flexCenter } from 'styles/utils';
-import Error from 'assets/ErrorImage.png';
-import PokemonBall from 'assets/PokemonBall.png';
-
-function LoadingSkeleton() {
-  return (
-    <Loading>
-      <SkeletonImage src={PokemonBall} alt="loading data" />
-    </Loading>
-  );
-}
-
-function ErrorSkeleton() {
-  return (
-    <Pokemon isCursor={false}>
-      <ErrorImage src={Error} alt="error image" />
-      <ErrorText>
-        Sorry man :( <br />
-        Please input another id!
-      </ErrorText>
-    </Pokemon>
-  );
-}
-
-const SkeletonImage = styled.img`
-  position: relative;
-  width: 5rem;
-  height: 5rem;
-  object-fit: contain;
-  animation: rotate 1s linear infinite;
-  margin: 0 auto;
-  top: 40%;
-  @keyframes rotate {
-    0% {
-      transform: rotate(0deg);
-    }
-    50% {
-      transform: rotate(360deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-`;
-
-const Loading = styled.div`
-  position: fixed;
-  top: 0;
-  height: 100%;
-  width: 100vw;
-  margin: 0;
-  background-color: rgba(0, 0, 0, 0.7);
-`;
-
-const ErrorImage = styled.img`
-  width: 18rem;
-  height: 18rem;
-  object-fit: contain;
-`;
-
-const ErrorText = styled.p`
-  margin-top: 5rem;
-  ${fonts.subtitle2};
-  color: ${colors.mint};
-`;
+import { LoadingSkeleton, ErrorSkeleton } from './Skeleton';
 
 export default function PokemonCard({ id }: { id: string }) {
   const { data, isLoading, isError, isSuccess } = usePokemon(id);
@@ -99,7 +36,7 @@ export default function PokemonCard({ id }: { id: string }) {
   );
 }
 
-const Pokemon = styled.div<{ isCursor: boolean }>`
+export const Pokemon = styled.div<{ isCursor: boolean }>`
   margin-top: 3rem;
   height: 40rem;
   padding: 2rem;
@@ -112,8 +49,8 @@ const Pokemon = styled.div<{ isCursor: boolean }>`
   ${(props) =>
     props.isCursor &&
     `&:hover {
-      background: palevioletred;
-      color: white;
+      background: ${colors.pink};
+      color: ${colors.white};
     }
     cursor: pointer;
     `}
---
+++ src/components/Main/Skeleton.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import Error from 'assets/ErrorImage.png';
+import PokemonBall from 'assets/PokemonBall.png';
+import { colors, fonts } from 'styles/theme';
+import { Pokemon } from './PokemonCard';
+
+export function LoadingSkeleton() {
+  return (
+    <Loading>
+      <SkeletonImage src={PokemonBall} alt="loading data" />
+    </Loading>
+  );
+}
+
+export function ErrorSkeleton() {
+  return (
+    <Pokemon isCursor={false}>
+      <ErrorImage src={Error} alt="error image" />
+      <ErrorText>
+        Sorry man :( <br />
+        Please input another id!
+      </ErrorText>
+    </Pokemon>
+  );
+}
+
+const Loading = styled.div`
+  position: fixed;
+  top: 0;
+  height: 100%;
+  width: 100vw;
+  margin: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+`;
+
+const SkeletonImage = styled.img`
+  position: relative;
+  width: 5rem;
+  height: 5rem;
+  object-fit: contain;
+  animation: rotate 1s linear infinite;
+  margin: 0 auto;
+  top: 40%;
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(360deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const ErrorImage = styled.img`
+  width: 18rem;
+  height: 18rem;
+  object-fit: contain;
+`;
+
+const ErrorText = styled.p`
+  margin-top: 5rem;
+  ${fonts.subtitle2};
+  color: ${colors.mint};
+`;
